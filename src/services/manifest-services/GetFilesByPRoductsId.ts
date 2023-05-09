2633c89fc2d88fb5db5fdf0acb355061ad7505e8
Carlos Casas <carloscasasramirez@gmail.com>
2023-05-09 17:34:59 -0500

    change

--- src/services/manifest-services/GetFilesByPRoductsId.ts
+++ src/services/manifest-services/GetFilesByPRoductsId.ts
@@ -5,44 +5,45 @@ import cloudinary from 'cloudinary'
 
 cloudinary.v2.config({
 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_APY_KEY,
-    api_secret: process.env.CLOUDINARY_APY_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_APY_KEY,
+  api_secret: process.env.CLOUDINARY_APY_SECRET
 
 })
 
 export class GetFilesByPRoductsId {
 
-    static getFiles = async (req: Request, res: Response) => {
+  static getFiles = async (req: Request, res: Response) => {
 
-        
-        const productId: string[] = (req.query.productId as string).split(",");
-        const productIdNumbers: number[] = [];
-        
-        productId.forEach((id: string) => {
-          const parsedId = Number(id);
-          if (!isNaN(parsedId)) {
-            productIdNumbers.push(parsedId);
-          }
-        });
-        
-        if (productIdNumbers.length === 0) {
-          res.status(400).send('Invalid productId parameter');
-          return;
-        }
-        
-        try {
-          const response = await Manifest.find({ productId: { $in: productIdNumbers, $ne: [productId[0]] } }).exec();
-          console.log(response);
-          res.send(response);
-        } catch (err) {
-          console.log(err);
-          res.status(500).send(err);
-        }
-        
-        
 
+    const productId: string[] = (req.query.productId as string).split(",");
+    const productIdNumbers: number[] = [];
+
+    productId.forEach((id: string) => {
+      const parsedId = Number(id);
+      if (!isNaN(parsedId)) {
+        productIdNumbers.push(parsedId);
+      }
+    });
+
+    if (productIdNumbers.length === 0) {
+      res.status(400).send('Invalid productId parameter');
+      return;
     }
+
+    try {
+      const response = await Manifest.find({
+        productId: { $in: productIdNumbers, $ne: [productId[0]] }
+      }).exec();
+      res.send(response);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+
+
+
+  }
 
 
 
