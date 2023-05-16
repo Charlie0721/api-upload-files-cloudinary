8b961a3998e8b47360faf663e7acfd57cd8dcc8b
Carlos Casas <carloscasasramirez@gmail.com>
2023-05-16 14:18:36 -0500

    new connection string

--- src/db.ts
+++ src/db.ts
@@ -1,31 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config();
-// class Database {
-
-
-//   constructor() {
-//     this.connect();
-//   }
-//   connect() {
-//     const MONGODB_URL = process.env.MONGODB_URL;
-//     if (!MONGODB_URL) {
-//       throw new Error('Environment variable not found MONGODB_URL');
-//     }
-//     mongoose
-//       .connect(MONGODB_URL)
-//       .then(() => {
-//         console.log('Connected to MongoDB database');
-//       })
-//       .catch((error) => {
-//         console.error('Connection error: ', error);
-//       });
-//   }
-// }
-
-// export default new Database();
-
-
 
 class Database {
   constructor() {
@@ -33,25 +9,25 @@ class Database {
   }
 
   connect() {
-    const { MONGODB_URL, MONGOHOST, MONGOPASSWORD, MONGOPORT, MONGOUSER } = process.env;
+    const {
+      MONGO_URL,
+      MONGOHOST,
+      MONGOPASSWORD,
+      MONGOPORT,
+      MONGOUSER,
+      MONGODB_DATABASE
+    } = process.env;
 
-    if (!MONGODB_URL || !MONGOHOST || !MONGOPASSWORD || !MONGOPORT || !MONGOUSER) {
-      throw new Error('Environment variables not found');
+    if (!MONGO_URL) {
+      throw new Error('Environment variable not found: MONGODB_URL');
     }
 
-    const connectionOptions = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      auth: {
-        user: MONGOUSER,
-        password: MONGOPASSWORD,
-      },
-      host: MONGOHOST,
-      port: MONGOPORT,
-    };
+    const connectionString = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}/${MONGODB_DATABASE}`;
 
     mongoose
-      .connect(MONGODB_URL, connectionOptions)
+      .connect(connectionString, {
+      
+      })
       .then(() => {
         console.log('Connected to MongoDB database');
       })
