63a3b3e25457b1982a01c2a61e762bbee3f86866
Carlos Casas <carloscasasramirez@gmail.com>
2023-05-16 14:01:16 -0500

    new class to connection

--- src/db.ts
+++ src/db.ts
@@ -1,24 +1,62 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+// class Database {
+
+
+//   constructor() {
+//     this.connect();
+//   }
+//   connect() {
+//     const MONGODB_URL = process.env.MONGODB_URL;
+//     if (!MONGODB_URL) {
+//       throw new Error('Environment variable not found MONGODB_URL');
+//     }
+//     mongoose
+//       .connect(MONGODB_URL)
+//       .then(() => {
+//         console.log('Connected to MongoDB database');
+//       })
+//       .catch((error) => {
+//         console.error('Connection error: ', error);
+//       });
+//   }
+// }
+
+// export default new Database();
+
+
+
 class Database {
-
-
   constructor() {
     this.connect();
   }
+
   connect() {
-    const MONGODB_URL = process.env.MONGODB_URL;
-    if (!MONGODB_URL) {
-      throw new Error('Environment variable not found MONGODB_URI');
+    const { MONGODB_URL, MONGOHOST, MONGOPASSWORD, MONGOPORT, MONGOUSER } = process.env;
+
+    if (!MONGODB_URL || !MONGOHOST || !MONGOPASSWORD || !MONGOPORT || !MONGOUSER) {
+      throw new Error('Environment variables not found');
     }
+
+    const connectionOptions = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      auth: {
+        user: MONGOUSER,
+        password: MONGOPASSWORD,
+      },
+      host: MONGOHOST,
+      port: MONGOPORT,
+    };
+
     mongoose
-      .connect(MONGODB_URL)
+      .connect(MONGODB_URL, connectionOptions)
       .then(() => {
         console.log('Connected to MongoDB database');
       })
       .catch((error) => {
-        console.error('Connection error: ', error);
+        console.error('Connection error:', error);
       });
   }
 }
